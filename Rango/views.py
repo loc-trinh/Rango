d49630334c5ae1ce21f21c435e3c6a919eca9430
Loc Trinh <loctrinh@mit.edu>
2015-12-25 00:51:38 -0800

    Testing template rendering and serving static files.

--- Rango/views.py
+++ Rango/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 
 def index(requests):
-    return HttpResponse("Hello, World!")
+    context = {'boldmessage': "Welcome to RANGO"}
+    return render(requests, "Rango/index.html", context)
+
+
+def about(requests):
+    html = "<p>This is the about page</p><a href='/rango'>Index</a>"
+    return HttpResponse(html)
--- webProject/settings.py
+++ webProject/settings.py
@@ -82,5 +82,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+)
