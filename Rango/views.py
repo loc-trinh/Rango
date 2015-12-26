7b3c0b6a28d23b5b7f2624883a84a8d8eb5a81fa
Loc Trinh <loctrinh@mit.edu>
2015-12-25 23:05:45 -0800

    Dynamically displaying categories and pages. Added slug fields for category clean URLs. Added top 5 pages viewed to index.

--- Rango/views.py
+++ Rango/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Rango.models import Category, Page
 
 
 def index(requests):
-    context = {'boldmessage': "Welcome to RANGO"}
+    context = {}
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context["categories"] = category_list
+
+    page_list = Page.objects.order_by('-views')[:5]
+    context["pages"] = page_list
+
     return render(requests, "Rango/index.html", context)
+
+
+def category(requests, category_name_slug):
+    context = {}
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+        context['category_name'] = cat.name
+
+        pages = Page.objects.filter(category=cat)
+        context["pages"] = pages
+
+        context["category"] = cat
+
+    except Category.DoesNotExist:
+        pass
+
+    return render(requests, "Rango/category.html", context)
 
 
 def about(requests):
