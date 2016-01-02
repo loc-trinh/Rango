86e07716c34952a476f66523dd8270f11380aebc
Loc Trinh <loctrinh@mit.edu>
2016-01-02 11:06:25 -0800

    Added tracking views to index and category pages. Redirecting pages from app.

--- Rango/views.py
+++ Rango/views.py
@@ -119,6 +119,18 @@ def search(request):
     return render(request, "Rango/search.html", {'result_list': result_list})
 
 
+def track_url(request):
+    if request.method == 'GET':
+        if 'page_id' in request.GET:
+            page_id = request.GET['page_id']
+            page = Page.objects.get(id=page_id)
+            page.views += 1
+            page.save()
+            return HttpResponseRedirect(page.url)
+        else:
+            return HttpResponseRedirect('/rango/')
+
+
 # def register(request):
 #     registered = False
 #
