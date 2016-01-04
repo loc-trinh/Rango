26ecb07f35102492e7c5c1584a3419a1ede15339
Loc Trinh <loctrinh@mit.edu>
2016-01-03 17:35:47 -0800

    Done!

--- webProject/settings.py
+++ webProject/settings.py
@@ -80,8 +80,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/' # You may find this is already defined as such.
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
