dbf03698289c7b1067ebd971948331f65ddf42e7
Loc Trinh <loctrinh@mit.edu>
2016-01-03 16:56:07 -0800

    Playing around with testing.

--- Rango/tests.py
+++ Rango/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from Rango.models import Category
 
-# Create your tests here.
+
+class CategoryMethodTests(TestCase):
+    def test_ensure_views_are_positive(self):
+        """
+        ensure_views_are_positive should results True for categories where views are zero or positive
+        """
+        cat = Category(name='test',views=-1, likes=0)
+        cat.save()
+        self.assertEqual((cat.views >= 0), False)
+
+    def test_slug_line_creation(self):
+         """
+         slug_line_creation checks to make sure that when we add a category an appropriate slug line is created
+         i.e. "Random Category String" -> "random-category-string"
+         """
+         cat = Category(name='Random Category String')
+         cat.save()
+         self.assertEqual(cat.slug, 'random-category-string')
